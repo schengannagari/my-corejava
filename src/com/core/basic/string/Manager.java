bc2db75c744eaed2ebe9561a756ce605048db963
Swaminath Chengannagari <swaminath87@gmail.com>
2024-02-13 15:49:09 +0530

    committed string basic examples

---
+++ src/com/core/basic/string/Manager.java
@@ -0,0 +1,48 @@
+package com.core.basic.string;
+
+public class Manager {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = str1;
+        String str3 = new String("hello");
+        String str4 = new String("hello").intern();
+        String str5 = str1 + " world";
+        String str6 = "hello world";
+        String str7 = String.join(" ", "hello",  "world");
+
+        System.out.print("Is both str1 and str2 are same?");
+        if (str1 == str2) {
+            System.out.println("\tYes");
+        } else {
+            System.out.println("\tNo");
+        }
+
+        System.out.print("Is both str2 and str3 are same?");
+        if (str2 == str3) {
+            System.out.println("\tYes");
+        } else {
+            System.out.println("\tNo");
+        }
+
+        System.out.print("Is both str1 and str4 are same?");
+        if (str2 == str4) {
+            System.out.println("\tYes");
+        } else {
+            System.out.println("\tNo");
+        }
+
+        System.out.print("Is both str5 and str6 are same?");
+        if (str5 == str6) {
+            System.out.println("\tYes");
+        } else {
+            System.out.println("\tNo");
+        }
+
+        System.out.print("Is both str6 and str7 are same?");
+        if (str7 == str6) {
+            System.out.println("\tYes");
+        } else {
+            System.out.println("\tNo");
+        }
+    }
+}
---
+++ src/com/core/basic/string/Manager2.java
@@ -0,0 +1,17 @@
+package com.core.basic.string;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class Manager2 {
+    public static void main(String[] args) {
+        String str = "Welcome to Java";
+        String substring1 = str.substring(11);
+        System.out.println("substring:\t"+substring1);
+
+        String reversedString = str.chars().mapToObj(c -> String.valueOf((char)c)).collect(Collectors.joining());
+        String reversed = str.chars().mapToObj(c -> String.valueOf((char)c)).collect(Collectors.collectingAndThen(Collectors.toList(), list -> {Collections.reverse(list); return String.join("", list);}));
+        System.out.println("Reversed String:\t"+reversedString);
+        System.out.println("reversed:\t"+ reversed);
+    }
+}
